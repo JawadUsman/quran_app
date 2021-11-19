7f2fd076f0e9a9f563c4377f22db37fb9720aa7c
jawadusman <jawafacer@gmail.com>
2021-11-19 10:31:10 +0400

    App update notification added

--- lib/pages/splash/splash_widget.dart
+++ lib/pages/splash/splash_widget.dart
@@ -1,5 +1,8 @@
 // @dart=2.11
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:in_app_update/in_app_update.dart';
 import 'package:quran_app/baselib/base_state_mixin.dart';
 import 'package:quran_app/baselib/base_widgetparameter_mixin.dart';
 import 'splash_store.dart';
@@ -13,12 +16,36 @@ class SplashWidget extends StatefulWidget with BaseWidgetParameterMixin {
 class _SplashWidgetState extends State<SplashWidget>
     with BaseStateMixin<SplashStore, SplashWidget> {
   final _store = SplashStore();
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> checkForUpdate() async {
+    InAppUpdate.checkForUpdate().then((info) {
+      print("Jawad update info: $info");
+      info.updateAvailability == UpdateAvailability.updateAvailable
+          ? () {
+              InAppUpdate.performImmediateUpdate()
+                  .catchError((e) => navigateToHomeScreen());
+            }
+          : navigateToHomeScreen();
+    }).catchError((e) {
+      print("Jawad update info error: $e");
+      navigateToHomeScreen();
+    });
+  }
+
   @override
   SplashStore get store => _store;
 
   @override
   void initState() {
     super.initState();
+    if (Platform.isAndroid) {
+      checkForUpdate();
+    } else
+      navigateToHomeScreen();
+  }
+
+  void navigateToHomeScreen() {
     Future.delayed(const Duration(milliseconds: 2000), () {
       setState(() {
         WidgetsBinding.instance.addPostFrameCallback((_) {
@@ -36,9 +63,7 @@ class _SplashWidgetState extends State<SplashWidget>
           decoration: BoxDecoration(
               image: DecorationImage(
                   image: AssetImage('assets/images/divya-quran.png'),
-                  fit: BoxFit.cover
-              )
-          ),
+                  fit: BoxFit.cover)),
         ),
       ),
     );
